9efa96429ec33efff255845ecdcb08a4b3277a37
Anton Bobrov <baverman@gmail.com>
2023-06-08 18:33:15 +0100

    release 0.10.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='buildbot_pipeline',
-    version='0.10.2',
+    version='0.10.3',
     url='https://github.com/baverman/buildbot_pipeline/',
     license='MIT',
     author='Anton Bobrov',
